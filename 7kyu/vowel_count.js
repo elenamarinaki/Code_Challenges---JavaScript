89660a77e26768dfcce506c4eec9322a86ae46ff
Elena <rhapsodyenmauve@gmail.com>
2021-07-30 23:10:27 +0100

    vowel count

--- 7kyu/vowel_count.js
+++ 7kyu/vowel_count.js
@@ -4,4 +4,24 @@
 
 // The input string will only consist of lower case letters and/or spaces.
 
+function getCount(str) {
+  var vowelsCount = 0;
 
+  // enter your majic here
+  for (var i = 0; i <= str.length; i++) {
+    if (
+      str[i] == 'a' ||
+      str[i] == 'e' ||
+      str[i] == 'i' ||
+      str[i] == 'o' ||
+      str[i] == 'u'
+    ) {
+      vowelsCount++;
+    }
+  }
+
+  return vowelsCount;
+}
+
+getCount('abracadabra');
+// 5
