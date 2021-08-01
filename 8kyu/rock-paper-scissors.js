dbf5d47cb21aa1024f67a1f5228022a73a7ea1b9
Elena <rhapsodyenmauve@gmail.com>
2021-08-01 02:39:11 +0100

    rock-paper-scissors

--- 8kyu/rock-paper-scissors.js
+++ 8kyu/rock-paper-scissors.js
@@ -7,3 +7,25 @@
 // rps('scissors','paper') // Player 1 won!
 // rps('scissors','rock') // Player 2 won!
 // rps('paper','paper') // Draw!
+
+const rps = (p1, p2) => {
+  if (p1 == p2) {
+    return 'Draw!';
+  } else if (p1 == 'scissors' && p2 == 'paper') {
+    return 'Player 1 won!';
+  } else if (p1 == 'paper' && p2 == 'rock') {
+    return 'Player 1 won!';
+  } else if (p1 == 'rock' && p2 == 'scissors') {
+    return 'Player 1 won!';
+  } else {
+    return 'Player 2 won!';
+  }
+};
+
+rps('rock', 'scissors');
+// p1
+rps('paper', 'scissors');
+// p2
+
+rps('paper', 'paper');
+// draw
