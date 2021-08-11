0d2f92b72b63c50c4a839359419a11600c9573a3
Elena <rhapsodyenmauve@gmail.com>
2021-08-12 00:45:09 +0100

    REST Parameters examples

--- Modern_JavaScript/restParameters.js
+++ Modern_JavaScript/restParameters.js
@@ -13,3 +13,15 @@ const sums = [sum(), sum(100), sum(2000, 1), sum(-500, -300)];
 sums;
 
 // Goal: [0, 100, 2001, -800];
+
+// ------------------------- EXAMPLES
+function addMany(toAdd, ...numbers) {
+  const result = [];
+  for (const n of numbers) {
+    result.push(n + toAdd);
+  }
+  return result;
+}
+addMany(2, 1, 7.7, 1000);
+
+// Result: [3, 9.7, 1002];
