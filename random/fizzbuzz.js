11e16aba6e90f49e81b5b25cb07e3da76d666dc5
Elena <rhapsodyenmauve@gmail.com>
2021-08-15 20:14:26 +0100

    fizzbuzz

---
+++ random/fizzbuzz.js
@@ -0,0 +1,13 @@
+function fizzbuzz(min, max) {
+  for (let i = min; i < max; i++) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+      console.log('buzz');
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
